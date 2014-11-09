ba90fd8ad3e7a4c0b92a073b9128d84aeb068766
Shemtek <christian@christiasssmbp4.speedport_w_921v_1_36_000>
2014-11-09 19:15:08 +0100

    Zugriff über DB

--- index.php
+++ index.php
@@ -1,48 +1,77 @@
 <?php
 
-spl_autoload_register(function ($class) {
-            include '' . str_replace('\\', '/', $class) . '.php';
-        });
+if (strpos($_SERVER["SERVER_NAME"], "www.geoip.local") !== false) define('SERVER', 0); 
+else define('SERVER', 1); 
 
-use GeoIp2\Database\Reader;
+define('CFG_USER', SERVER ? 'root'	: 'root');
+define('CFG_PASS', SERVER ? '': '');
+define('CFG_DB', SERVER ? 'geoip': 'geoip');
+define('CFG_MYSQL',SERVER ? ':/cloudsql/canvas-epigram-758:geoip'	: '127.0.0.1:3306');
 
-// This creates the Reader object, which should be reused across
-// lookups.
 try {
-    $filename = __DIR__ . '/MMDB/GeoLite2-City.mmdb';
-    $reader = new Reader($filename);
-     /*if(!file_exists($filename))
-     {
-         $reader = new Reader('gs://mmdb/GeoLite2-City.mmdb');
-         //file_put_contents($filename, file_get_contents('gs://mmdb/GeoLite2-City.mmdb'));
-     }
-     else 
-     {
-         $reader = new Reader($filename);// Replace "city" with the appropriate method for your database, e.g.,
-     }*/
-
-// "country".
+    
+    if(!SERVER)
+    {
+    $ipnum = 33996344;
+ 
+    $o1 = ($ipnum / 16777216 ) % 256;
+    $o2 = ($ipnum / 65536    ) % 256;
+    $o3 = ($ipnum / 256      ) % 256;
+    $o4 = ($ipnum            ) % 256;
+ 
+    print( $o1.'.'. $o2.'.'. $o3.'.'. $o4 );
+    }
+    // DB Connection
+    // Using MySQL API (connecting from APp Engine)
+    $conn = mysql_connect(CFG_MYSQL,
+    CFG_USER, // username
+    CFG_PASS // password
+    );
+    
+    
+    mysql_select_db(CFG_DB, $conn);
+    
+    // "country".
     if(isset($_GET["ip"]))$ip = $_GET["ip"];
     if (strlen($ip) == 0) $ip = $_SERVER["REMOTE_ADDR"];
     $ip = filter_var($ip, FILTER_VALIDATE_IP);
     if ($ip) {
-        $record = $reader->city($ip);
-
-//print($record->country->isoCode . "\n"); // 'US'
-//print($record->country->name . "\n"); // 'United States'
-//print($record->country->names['zh-CN'] . "\n"); // '美国'
-//print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
-//print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
-
-        print($record->city->name . "\n"); // 'Minneapolis'
-//print($record->postal->code . "\n"); // '55455'
-//print($record->location->latitude . "\n"); // 44.9733
-//print($record->location->longitude . "\n"); // -93.2323
+        
+        $query = "SELECT locId FROM LocId WHERE INET_ATON('".$ip."') BETWEEN StartIpNum AND EndIpNum LIMIT 1;";
+        
+        $result = mysql_query($query, $conn);
+        if(mysql_error()) throw new Exception;
+        if(isset($result))
+        {
+            $locid = mysql_fetch_array($result, MYSQL_ASSOC);
+            if(isset($locid['locId']))
+            {
+               $query = "SELECT city FROM City WHERE locId = ".$locid['locId']." LIMIT 1;"; 
+               $result = mysql_query($query, $conn);
+               if(mysql_error()) throw new Exception;
+                if(isset($result))
+                {
+                    $city = mysql_fetch_array($result, MYSQL_ASSOC);
+                }
+            }
+        }
+        
+        
     } else {
         throw new Exception;
     }
+    
+    
+   
+
+
+
 } catch (Exception $e) {
-    print('In deiner Nähe '.$_SERVER["REMOTE_ADDR"]);
+    print('In deiner N&auml;he');
     print $e->getMessage();
+    print mysql_error();
 }
 
+if(isset($city)) print_r($city);
+
+mysql_close($conn);
